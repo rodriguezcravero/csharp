3ba80db49e7fbdcf6644f30a4ad964226f16254c
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-07-12 18:19:13 -0300

    proyecto juego ahorcado

---
+++ Programas propios/Proyecto Hangman - Udemy/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Proyecto_Hangman___Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Variables*/
+            Random random = new Random(); //Para acceder a un numero random, primero hay que instanciar un objeto de tipo Random.
+            int intentos = 8; //Designa la cantidad de intentos como variable global
+            bool ganaste = false; //Booleano global que sólo se vuelve true cuando se adivina toda la palabra
+            char letra; //Variable que alojará la letra ingresada cada vuelta. SE PODRIA MEJORAR HACIENDO VALIDACION DE SOLO LETRAS
+            string repetidas = ""; //Para solamente usar tipos básicos de datos, guardo las letras repetidas en un string. SE PODRIA MEJORAR USANDO UNA COLECCION DINAMICA
+            string[] palabras = new string[] { "palabra", "zodiaco", "carpincho", "abecedario", "murcielago", "espada", "computadora", "oceano", "hueste", "vecindario" }; //Palabras que serán seleccionadas de manera random. Podria haber N palabras.
+
+            string palabraSeleccionada = palabras[random.Next(palabras.Length)]; //Se elige una palabra del array de palabras.
+            string palabraOculta = ""; //String que será del mismo largo de la palabra seleccionada pero con asteriscos.
+
+            for (int i = 0; i < palabraSeleccionada.Length; i++) palabraOculta += '*'; //Lleno el string de asteriscos, que será mostrado en pantalla.
+
+            /*Comienza el juego*/
+            Console.WriteLine("**JUEGO DE AHORCADO**\n");
+            while (intentos > 0 && !ganaste) //El juego puede terminar porque se terminaron los intentos (perdió) o se adivinó la palabra (ganó);
+            {
+                Console.WriteLine($"Tiene {intentos} intentos. La palabra a adivinar es: {palabraOculta}"); // Para imprimir los asteriscos (array de caracteres) uso una funcion con un for que los imprime. Si se adivinan letras, se muestran en lugar de los asteriscos.
+                Console.Write("Ingrese una letra: ");
+                letra = char.Parse(Console.ReadLine()); //Opcion para leer un caracter. Se podría hacer de mejores formas para validar que sólo se ingresen letras.
+                if (letraValida(ref repetidas, letra)) //Lo primer que hago es chequear que se ingrese una letra válida, es decir, una letra que no se haya ingresado previamente.
+                {
+                    if (comprobarLetra(palabraSeleccionada, letra)) //Si la letra es nueva, se chequea que coincida con alguna de la palabra a adivinar
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Muy bien! ");
+                        actualizarAsteriscos(palabraSeleccionada, letra, ref palabraOculta, out ganaste); //Funcion que cambia cada asterisco por ocurrencia de la letra
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Esa letra no está en la palabra");
+                        intentos--; //Si la letra es nueva pero no coincide, se pierde un intento.
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Ya ingresaste esa letra previamente. Perdiste un intento.");
+                    intentos--; //Si la letra ya fue ingresada antes, se pierde un intento.
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            if (ganaste) Console.WriteLine($"Ganaste!! La palabra era {palabraSeleccionada}");
+            else Console.WriteLine($"Se acabaron los intentos y perdiste, la palabra era {palabraSeleccionada}");
+
+            Console.ReadKey();
+
+        }
+
+        public static bool letraValida(ref string repetidas, char letra)
+        {
+            //se recorre el string que aloja las letras repetidas. En tal caso, se retorna falso. Sino, se agrega al string.
+            for (int i = 0; i < repetidas.Length; i++) if (repetidas[i] == letra) return false;
+            repetidas += letra;
+            return true;
+        }
+
+        public static bool comprobarLetra(string palabra, char letra)
+        {
+            foreach (char l in palabra) if (l == letra) return true;
+            return false;
+        }
+
+        public static void actualizarAsteriscos(string palabraSeleccionada, char letra, ref string palabraOculta, out bool ganaste)
+        {
+            //Primero se recorre cada letra de la palabra para saber la posición del match. Luego se actualizan los asteriscos por la letra en esa posición.
+            for (int i = 0; i < palabraSeleccionada.Length; i++)
+            {
+                if (palabraSeleccionada[i] == letra)
+                {
+                    palabraOculta = palabraOculta.Remove(i, 1);
+                    palabraOculta = palabraOculta.Insert(i, letra.ToString());
+                }
+            }
+            //Si ya no quedan asteriscos, el jugador ganó.
+            ganaste = palabraOculta.Contains('*') ? false : true;
+        }
+    }
+}
---
+++ Programas propios/README.md
@@ -0,0 +1,25 @@
+# C#
+
+## :white_check_mark: Keywords :key: Interfaces, Async, Delegados, Genéricos, POO, Herencia, Polimorfismo, Encapsulamiento, Clases y Propiedades, Excepciones.
+
+### Proyectos del dominio .NET :computer:
+
+En 2º año de la Facultad de Informática de la UNLP se cursa la materia "Seminario de lenguajes", y opté por hacer el de C#.
+La materia recorre los conceptos básicos del lenguaje, en el entorno de la multiplataforma de Microsoft: **NetCore**
+
+**Algunos de los temas, enseñados a través de proyectos de consola en el seminario, son:**
+- Fundamentos del lenguaje, tipos y estructuras de datos, operadores, estructuras de control.
+- Sistema (unificado) de tipos, boxing/unboxing, tipos valor y referencia. 
+- Arreglos, arreglos de arreglos, matrices, strings y StringBuilder, formato de cadenas interpoladas, enumerativos.
+- Métodos, expression-bodied, expresiones lambda, parámetros in, ref, out, params. Var, object, dynamic. Extensión de métodos.
+- Colecciones y sus propiedades: ArrayList, Stack, Queue, HashTable.
+- Manejo de excepciones: bloques try/catch/finally, propagación, throw, creación de excepciones propias.
+- **POO:** Clases, miembros (campos, métodos, constructores, propiedades, indizadores, eventos, operadores, etc.)
+- Clases y miembros estáticos y abstractos, utility classes, sobrecarga de distintos miembros, null-coalescing.
+- Encapsulamiento, modificadores de acceso, encadenamiento de métodos.
+- Derivación y Herencia. Polimorfismo, métodos virtuales e invalidación, enlace dinámico. Principio OPEN/CLOSE.
+- Implementación y utilización de **Interfaces**, IComparable e IComparer, IEnumerable e IEnumerator. Iteradores, yield.
+- StreamReader y StreamWriter, Clases Path, File, Info. Manipulación de datos con archivos de texto, instrucción using, IDisposable.
+- **Delegados y eventos**, Action, Func, EventHandler y EventArgs, Event.
+- **Genéricos**, tipos, métodos, clases, interfaces y delegados genéricos, cláusula where, 
+- **Programación asincrónica:** patrón TAP, clase Task, async/await, tareas anidadas, Task.Factory, etc.
